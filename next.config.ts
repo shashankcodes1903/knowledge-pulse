426851a21eb9b92c29cce426f247860205ee07f8
Shubham-404 <shubh.shubhamkrsingh@gmail.com>
2026-09-11 03:38:37 +0530

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "standalone",
+  // output: "standalone",  // throws deployment error on vercel! keep it OFF.
 };
 
 export default nextConfig;
